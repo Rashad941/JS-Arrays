c7975aa389ace23a7435a8a0e62f1e94977456a6
Michael Ethan Rashad-Nakpan <methan941@gmail.com>
2025-04-13 00:09:12 +0000

    Add files via upload

---
+++ arrays-test.js
@@ -0,0 +1,34 @@
+//Creating an array
+
+const myName = ['Michael', 'Ethan', 'Rashad-Nakpan']
+
+//calling a value from an array
+
+myName[0, 1];
+
+//adding an array inside of an array
+
+const addingArrays = ['Ishfak', 'Kwabena', ['Kobby', 'Fak']];
+
+//calling values inside of a multidimentional array
+//calling Kwabena and Fak
+
+addingArrays[1], addingArrays[2][1];
+
+//adding a value to an to the end of an array. Ref. the first array
+ 
+myName.push ('gangster');
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
